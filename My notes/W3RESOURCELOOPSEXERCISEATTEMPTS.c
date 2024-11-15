429f93b694370f5f72bd200dd0a8e5f165232300
GooberXD <127967566+GooberXD@users.noreply.github.com>
2024-11-15 12:58:48 +0800

    Update W3RESOURCELOOPSEXERCISEATTEMPTS.c
    yipee

--- My notes/W3RESOURCELOOPSEXERCISEATTEMPTS.c
+++ My notes/W3RESOURCELOOPSEXERCISEATTEMPTS.c
@@ -128,3 +128,180 @@ int main(){
     return 0;
 }
 
+
+// 6. Write a program in C to display the multiplication table for a given integer.
+// Test Data :
+// Input the number (Table to be calculated) : 15
+// Expected Output :
+// 15 X 1 = 15
+// ...
+// ...
+// 15 X 10 = 150
+
+#include <stdio.h>
+
+int main(){
+    int num;
+
+    printf("Input number for table: ");
+    scanf("%d", &num);
+
+    for(int i = 1; i <= 10; i++){
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+    
+    return 0;
+}
+
+// 7. Write a program in C to display the multiplier table vertically from 1 to n.
+// Test Data :
+// Input upto the table number starting from 1 : 8
+// Expected Output :
+// Multiplication table from 1 to 8
+// 1x1 = 1, 2x1 = 2, 3x1 = 3, 4x1 = 4, 5x1 = 5, 6x1 = 6, 7x1 = 7, 8x1 = 8
+// ...
+// 1x10 = 10, 2x10 = 20, 3x10 = 30, 4x10 = 40, 5x10 = 50, 6x10 = 60, 7x10 = 70, 8x10 = 80 
+
+#include <stdio.h>
+
+int main(){
+    int num;
+
+    printf("Input up to the table number starting from 1: ");
+    scanf("%d", &num);
+
+    printf("Multiplication table from 1 to %d\n", num);
+
+    for(int i = 1; i <= num; i++){
+        
+        for(int j = 1; j <= 10; j++){
+            printf("%d x %d = %d, ", i, j, i * j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
+// 8. Write a C program to display the n terms of odd natural numbers and their sum.
+// Test Data
+// Input number of terms : 10
+// Expected Output :
+// The odd numbers are :1 3 5 7 9 11 13 15 17 19
+// The Sum of odd Natural Number upto 10 terms : 100 
+
+#include <stdio.h>
+
+int main(){
+    int terms, oddsum = 0;
+
+    printf("Input number of terms: ");
+    scanf("%d", &terms);
+
+    printf("The odd numbers are:\n");
+
+    int oddterms = terms * 2;
+
+    for(int i = 1; i <= oddterms; i++){
+        if (i % 2 != 0){
+            printf("%d ", i);
+            oddsum += i;
+        }
+
+    }
+    printf("\nThe sum of odd natural numbers up to %d terms: %d", terms, oddsum);
+
+    return 0;
+}
+
+// 9. Write a program in C to display a pattern like a right angle triangle using an asterisk.
+
+// The pattern like :
+
+// *
+// **
+// ***
+// ****
+
+#include <stdio.h>
+
+int main(){
+    int n;
+
+    printf("Layers to print: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++){
+        
+        for(int j = 1; j <= i; j++){
+            printf("*");
+        }
+    
+    printf("\n");
+
+    }
+
+    return 0;
+}
+
+// 10. Write a C program to display a pattern like a right angle triangle with a number.
+
+// The pattern like :
+
+// 1
+// 12
+// 123
+// 1234
+
+#include <stdio.h>
+
+int main(){
+    int n;
+
+    printf("How many layers to print with numbers? ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++){
+
+        for(int j = 1; j <= i; j++){
+            printf("%d", j);
+        }
+
+    printf("\n");
+
+    }
+
+    return 0;
+}
+
+// 11. Write a program in C to make such a pattern like a right angle triangle with a number which will repeat a number in a row.
+
+// The pattern like :
+
+//  1
+//  22
+//  333
+//  4444
+
+#include <stdio.h>
+
+int main(){
+    int n;
+
+    printf("How many layers to print with numbers? ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++){
+
+        for(int j = 1; j <= i; j++){
+            printf("%d", i);
+        }
+
+    printf("\n");
+
+    }
+
+    return 0;
+}
+
